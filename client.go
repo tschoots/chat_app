51f0f2ee90157477da352689682aa5b8ddcf0374
Ton Schoots <ton@maiastra.com>
2016-02-12 00:54:17 +0100

    update client

--- client.go
+++ client.go
@@ -16,3 +16,22 @@ type client struct {
 	room *room
 }
 
+func (c *client) read() {
+	for{
+		if _, msg := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		}else{
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
